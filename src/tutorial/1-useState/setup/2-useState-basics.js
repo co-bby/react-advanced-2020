ec1f4552d5e319b968203dd8b7e14310ba854fe4
co-bby <nkumsagoefredrick@gmail.com>
2021-10-27 15:57:26 +0000

    use State

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,30 @@
 import React, { useState } from 'react';
-
+// components should always start with an uppercase
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('cobby'))
+  // const value = useState(1)[0];
+  // const handler= useState(1)[1];
+  // console.log(value, handler)
+  const[text,setText]= useState('heather nix');
+ 
+ const handleClick =() => {
+   if (text === 'Heather Nix') {
+     setText('Cereal')
+
+   }
+   else {
+     setText('Heather Nix')
+   }
+   
+ }
+ 
+ return (<React.Fragment>
+    <h1>{text}</h1>
+    <button className="btn" onClick={handleClick}> Change Title</button>
+  </React.Fragment>
+
+  );
+  
 };
 
 export default UseStateBasics;
