351aad59b249ecb77c3e7e066d51382b06a3a2e2
co-bby <nkumsagoefredrick@gmail.com>
2021-11-04 00:11:52 +0000

    useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,26 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+const [value, setValue]= useState(0);
+useEffect(() => {
+console.log('call useEffect')
+if(value>=1){
+  
+  document.title= `New Messages is ${value}`;
+}
+}, [value])
+useEffect(() =>{
+  console.log('hello world')
+},[value])
+
+console.log('render component')
+  return (
+  <>
+<h1>{value}</h1>
+<button onClick={() =>setValue(value + 1) } className="btn">click Me</button>
+
+  </>
+  );
 };
 
 export default UseEffectBasics;
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,22 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers]= useState([]);
+
+  const getUsers= async () => {
+    const response =await fetch(url)
+    const users= await response.json()
+
+    // setUsers(users);
+    console.log(users)
+  }
+  useEffect(() => {
+    getUsers();
+  })
+  return( <>
+    <h3>Users</h3>
+  </>
+  );
 };
 
 export default UseEffectFetchData;
